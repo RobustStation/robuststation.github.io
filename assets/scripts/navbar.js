4df61d2d3c608cc5418091a6a31dc405cbf6f382
ToasterPanic <me@owouw.us>
2025-08-07 16:33:42 -0400

    Widgetbot crate, impulse restyling

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
     <span class="teenyfix">.</span> RobustStation <span class="teenyfix">.</span>
     </a></h1>
             <div style="text-align: center;">
-                <a href="https://discord.gg/25pyCXpZU3">Discord</a> | <a href="/rules">Rules</a> | <a href="/rules#rp">Roleplay Guidelines</a> | <a href="/rules#admin">Admin Guidelines</a>
+                <a href="https://discord.gg/25pyCXpZU3">Discord</a> | <a href="#rules">Rules</a> | <a href="#rp">Roleplay Guidelines</a> | <a href="#admin">Admin Guidelines</a>
             </div>`
 })
